38d013c8c46200fbf1c4d4445524390cfd648c74
meiji163 <mysatellite99@gmail.com>
2021-12-05 09:31:50 -0800

    silence output

--- status.sh
+++ status.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+
+help() {
+    cat << EOF 
+Usage: $0 [-n NUM_STATUSES]
+
+Set DISCORD_TOKEN to post to discord
+EOF
+}
+
+n=250
+while getopts 'n:h' flag; do
+    case "${flag}" in
+        n) n=${OPTARG} ;;
+        h) help
+           exit 0 ;;
+        *) exit 1 ;; 
+    esac
+done
+
+# change these to your own word files
 adj_file="english-adjectives.txt"
 words_file="words_alpha.txt"
-
 if [[ ! -e "$adj_file" ]]; then
     wget https://gist.githubusercontent.com/hugsy/8910dc78d208e40de42deb29e62df913/raw/eec99c5597a73f6a9240cab26965a8609fa0f6ea/english-adjectives.txt 
 fi 
@@ -11,13 +30,12 @@ fi
 
 rand_words(){
     # no i wont do it in bash tyvm 
-    n=100
     words="$( python -c "from random import choice
 with open(\"$words_file\",'r') as f: 
     words = [w.strip(\"\n\") for w in f.readlines()]
 with open(\"$adj_file\",'r') as f: 
     adjs = [w.strip(\"\n\") for w in f.readlines()]
-for _ in range($n):
+for _ in range($1):
     print(f\"{choice(adjs)} {choice(words)}\" )")"
 
     fzf --ansi <<< "$words"
@@ -30,6 +48,11 @@ post_status(){
         exit 1
     fi
 
+    if [[ -z "$DISCORD_TOKEN" ]]; then
+        echo "DISCORD_TOKEN env variable not set"
+        exit 1
+    fi
+
     curl https://discord.com/api/v9/users/@me/settings \
     -X 'PATCH' \
     -H "authorization: $DISCORD_TOKEN" \
@@ -38,7 +61,7 @@ post_status(){
     -H 'accept: */*' \
     --data-raw "{\"custom_status\":{\"text\":\"$status\"}}" \
     --silent \
-    --write-out '%{http_code}'
+    --write-out '%{http_code}' 
 }
 
-rand_words | post_status
+rand_words $n | post_status 1>&/dev/null
